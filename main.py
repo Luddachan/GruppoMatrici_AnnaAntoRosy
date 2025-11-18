4b95a604f879aeea934cf501cb4b7d368d150ce3
Rohesia <rosy.giudice56@gmail.com>
2025-11-18 17:28:14 +0100

    esercizio di gruppo 18-11
    Matrici

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from GestoreMatrice import GestoreMatrice
 from sottoclassi import *
-matrice = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+matrice = [
+    [1, 5, -4],
+    [3, 2, 1]
+]
+
+# --- Test GestoreMatrice ---
 gestore = GestoreMatrice(matrice)
+print("Matrice originale")
+
 gestore.stampa_matrice()
-if gestore.valida_matrice():
-    print("La matrice è valida.")
-
-matrice_trasposta = 
-
-trasponi_matrice = ElaboratoreMatematico(matrice).trasponi()
-print("Matrice Trasposta:")
+print(gestore.valida_matrice())
 
 
+# --- Test ElaboratoreMatematico ---
+elaboratore = ElaboratoreStatistico(matrice)
+print("Il valore massimo della matrice è: ", elaboratore.trova_massimo())
+print("Valore medio della riga 0 ", elaboratore.media_riga(0))
+
+# --- Test Filtro Matrice ---
+filtro = FiltroMatrice(matrice)
+filtro.azzera_negativi()
+
+print("Matrice azzerata: ")
+b = filtro.stampa_matrice()
+
+a = filtro.appiattisci()
+print("Matrice appiattita: ", a)
--- sottoclassi.py
+++ sottoclassi.py
@@ -1,25 +1,52 @@
+# classe derivate
+
 #Sottoclasse per le operazioni matematiche: Eredita da GestoreMatrice.
 from GestoreMatrice import GestoreMatrice
+# definizione classe elaboratore matematico 
 class ElaboratoreMatematico(GestoreMatrice):
     def trasponi(self):
-        if not self.valida_matrice():
-            return None
-        if not self.dati or not self.dati[0]: #Controlla se la matrice è vuota
-            return []
-        N = len(self.dati)
-        M = len(self.dati[0])
-        matrice_trasposta = [[0] * N for _ in range(M)] #Crea una matrice vuota di dimensioni MxN
-        for i in range(N):
-            for j in range(M):
-                matrice_trasposta[j][i] = self.dati[i][j]
-        print("\nTrasposizione completata.")
-        return matrice_trasposta
+        righe = len(self.dati)
+        colonne = len(self.dati[0])
+        trasposta = [[self.dati[i][j]] for i in range (righe) for j in range (colonne)]
+        return trasposta 
+    
     def moltiplica_per_scalare(self, k):
-        if not self.valida_matrice():
-            return None
-        nuova_matrice = []
+        return [[elemento * k for elemento in riga] for riga in self.dati]
+    
+    
+# trova il massimo della matrice 
+class ElaboratoreStatistico(GestoreMatrice):
+    def trova_massimo(self):
+        massimo = self.dati[0][0]
         for riga in self.dati:
-            nuova_riga = [elemento * k for elemento in riga]
-            nuova_matrice.append(nuova_riga)
-        print(f"\nMoltiplicazione per lo scalare {k} completata.")
-        return nuova_matrice
+            for valore in riga:
+                if valore > massimo:
+                    massimo = valore 
+        return massimo 
+    
+    # trova valore media 
+    def media_riga(self, indice_riga):
+        riga= self.dati[indice_riga]
+        return sum(riga) / len(riga)
+    
+# classe filtro matrice 
+class FiltroMatrice(GestoreMatrice):
+    def azzera_negativi(self):
+        for i in range(len(self.dati)):
+            for j in range(len(self.dati[i])):
+                if self.dati[i][j] < 0:
+                    self.dati[i][j] = 0
+                    
+                    
+    def appiattisci(self):
+        lista = []
+        for riga in self.dati:
+            for elemento in riga:
+                lista.append(elemento)
+        return lista
+                    
+
+    
+    
+
+        
